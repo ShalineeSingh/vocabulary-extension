2a79f3c3cd2b6a77bd36515a8804b77f81b74b4e
shalinee.singh <shalinee.singh@coviam.com>
2017-11-24 08:43:25 +0530

    testing for wordsapi

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const axios = require('axios');
 const http = require('http');
 const path = require('path');
 
-const publicPath = path.join(__dirname +  '/../public');
+const publicPath = path.join(__dirname + '/../public');
 console.log(publicPath);
 var app = express();
 var server = http.createServer(app);
 
+var getWord = () => {
+  axios.get('https://wordsapiv1.p.mashape.com/words/bump/also', {
+    headers: {
+      'X-Mashape-Key': 'XbG1PmNfMLmsh36XgJ4N0c2pVYmop1dLc41jsnEZChwK4idHQG',
+      'Accept': 'application/json'
+    }
+  }).then((response) => {
+    console.log(response.data);
+  });
+};
+
+getWord();
 app.use(express.static(publicPath));
 server.listen(3000, () => {
-	console.log(`Server started on port 3000`);
+  console.log(`Server started on port 3000`);
 });
